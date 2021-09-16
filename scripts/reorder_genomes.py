b61c34fef5d1fa0340ecf67c662b325f5430f825
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-09-16 11:29:35 +0100

    Small update to reorder_genomes

--- scripts/reorder_genomes.py
+++ scripts/reorder_genomes.py
@@ -7,6 +7,7 @@ import logging
 import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+
 def get_args():
     parser = argparse.ArgumentParser(description='Reorder genomes WRT to an ordering.')
     parser.add_argument('--input_dir', type=str, help='Path to a dir where each file is a genome', required=True)
@@ -14,6 +15,7 @@ def get_args():
     parser.add_argument('--output_dir', type=str, help='Where to save the reordered genomes', required=True)
     args = parser.parse_args()
     return args
+
 
 def main():
     args = get_args()
@@ -24,6 +26,7 @@ def main():
     output_dir.mkdir(parents=True, exist_ok=False)
     reorder_genomes(input_dir, order, output_dir)
 
+
 def get_order_to_path(file, order):
     order_to_path = {}
     for order_id, order_str in enumerate(order):
@@ -31,18 +34,21 @@ def get_order_to_path(file, order):
             order_to_path[order_id] = file
     file_was_matched_to_at_most_one_entry = len(order_to_path) <= 1
     if not file_was_matched_to_at_most_one_entry:
-        raise Exception(f"Error for {file}")
+        raise Exception(f"Error for {file} - it was matched to several entries: {order_to_path}")
     return order_to_path
+
 
 def get_random_name(length=40):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for _ in range(length))
+
 
 def get_n_random_sorted_names(n):
     random_names = set()
     while len(random_names) < n:
         random_names.add(get_random_name())
     return sorted(list(random_names))
+
 
 def reorder_genomes(input_dir, order, output_dir):
     order_to_path = {}
@@ -59,4 +65,6 @@ def reorder_genomes(input_dir, order, output_dir):
         logging.info(f"{source} -> {dest}")
         os.symlink(str(source), str(dest))
 
-main()
+
+if __name__ == "__main__":
+    main()
