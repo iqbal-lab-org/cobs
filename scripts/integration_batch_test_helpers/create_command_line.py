9b5a6d0f33e18eb0f35ec7273438b33a5e275aa3
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-06-15 23:33:21 +0100

    Updating create_command_line.py

--- scripts/integration_batch_test_helpers/create_command_line.py
+++ scripts/integration_batch_test_helpers/create_command_line.py
@@ -15,6 +15,7 @@ for batch in batches:
         f"{cobs} classic-construct -T 1 {out_dir}/reordered_assemblies/{batch} {out_dir}/COBS_out/{batch}.cobs_classic")
     print(
         f"{cobs} generate-queries -T 1 -n {nb_negative_queries} -p {nb_positive_queries} -k {kmer_size} -o {out_dir}/{batch}_queries.fa {out_dir}/reordered_assemblies/{batch}")
+    print(f"sed -i 's/N/A/g' {out_dir}/{batch}_queries.fa")
     print(
         f"{cobs} query -i {out_dir}/COBS_out/{batch}.cobs_classic -f {out_dir}/{batch}_queries.fa --load-complete -T 1 > {out_dir}/{batch}.query_results.load_complete.txt")
     print(
