cfeeb57aa25d5ec15a97020f1a07fa3b7894e222
Leandro Ishi <leandro.ishi.lima@gmail.com>
2022-07-18 15:19:24 +0100

    Adding -DBOOST flag to force Boost

--- cobs/util/fs.hpp
+++ cobs/util/fs.hpp
@@ -9,15 +9,23 @@
 #ifndef COBS_UTIL_FS_HEADER
 #define COBS_UTIL_FS_HEADER
 
+#ifdef COBS_USE_BOOST
+
 #include <boost/filesystem.hpp>
-
 namespace cobs {
-
 namespace fs = boost::filesystem;
 using boost::system::error_code;
-
 } // namespace cobs
 
+#else
+
+#include <experimental/filesystem>
+namespace cobs {
+  namespace fs = std::experimental::filesystem;
+  using std::error_code;
+} // namespace cobs
+
+#endif
 
 #endif // !COBS_UTIL_FS_HEADER
 
